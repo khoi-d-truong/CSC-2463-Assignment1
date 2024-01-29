cecaf8ad0332668943a51229869ed4f66d992f15
khoi-d-truong <khoi.truong98@gmail.com>
2024-01-29 16:58:09 -0600

    Update sketch.js

--- assignment1-3/sketch.js
+++ assignment1-3/sketch.js
@@ -1,7 +1,30 @@
-function setup() {
-  createCanvas(400, 400);
+
+function setup()
+{
+  createCanvas(750,400);
+  angleMode(DEGREES);
+  
 }
 
-function draw() {
-  background(220);
+function draw()
+{
+  background(0);
+  //pacman
+  fill('yellow');
+  arc(200,200,300,300,225,135, PIE);
+  
+  //ghost
+  fill('red');
+  noStroke();
+  rect(400,150,300,200);
+  arc(550,150,300,230,180,0)
+  
+  //eyes  
+  fill('white');
+  circle(480,180,100);
+  circle(620,180,100);
+  fill('blue');
+  circle(480,180,75);
+  circle(620,180,75);
 }
+
