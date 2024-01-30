f1f7d1abf71deb99193fd819bdb441ccb83ed306
khoi-d-truong <khoi.truong98@gmail.com>
2024-01-30 09:04:00 -0600

    Update sketch.js

--- assignment1-4/sketch.js
+++ assignment1-4/sketch.js
@@ -1,7 +1,38 @@
-function setup() {
-  createCanvas(400, 400);
+function setup()
+{
+  createCanvas(500,500);
+
 }
 
-function draw() {
-  background(220);
+function draw()
+{
+  background('rgb(0,0,128)');
+
+  //circle
+  fill('rgb(0,100,0)');
+  stroke('white');
+  strokeWeight(5);
+  circle(250,250,300);
+  
+  //star
+  fill('red');
+  beginShape();
+    vertex(250,100);
+    vertex(294,189);
+  
+    vertex(392,204);
+    vertex(321,273);
+  
+    vertex(338,371);
+    vertex(250,325);
+  
+    vertex(162,371);
+    vertex(179,273);
+  
+    vertex(108,204);
+    vertex(206,189);
+  
+    vertex(250,100);
+  
+  endShape(CLOSE);
 }
